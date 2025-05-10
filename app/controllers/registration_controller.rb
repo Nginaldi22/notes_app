bcdbc083e8efe718ef65986317a4db7750831edc
Nginaldi22 <nginaldi@gmail.com>
2025-05-10 13:13:02 -0400

    homepage

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -7,6 +7,14 @@ class RegistrationController < ApplicationController
   end
 
   def create
+     @registration = Registration.new(registration_parms)
+  if @registration.save
+    @registration.create_homepage(title: "Welcome #{@registration.name}!", description: "Your personal dashboard.")
+    session[:user_id] = @registration.id
+    redirect_to homepage_show_path
+  else
+    render :new
+  end
   end
 
   def login
@@ -14,6 +22,6 @@ class RegistrationController < ApplicationController
 
   private
 def registration_parms
-  params.require(:registration).permit(:name, :email, :password)
+  params.require(:registration).permit(:name, :email, :password, :password_confirmation)
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get "homepage/show"
   root "registration#sign_in"
   post "registration/login", to: "registration#login"
   get  "registration/new", to: "registration#new"
   post "registration", to: "registration#create"
+  get "homepage", to: "homepage#show"
   get "up" => "rails/health#show", as: :rails_health_check
 end
