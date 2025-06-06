8c5b99bbf655c518777f584e0d8712034049e1f4
Nginaldi22 <nginaldi@gmail.com>
2025-06-06 12:47:36 -0400

    styling upload and some final touches

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,17 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "form_validation"
+/*
+if i want to go back and add flash and stuff this is how
+<% if note.errors.any? %>
+    <div style="color: red">
+      <h2><%= pluralize(note.errors.count, "error") %> prohibited this note from being saved:</h2>
+
+      <ul>
+        <% note.errors.each do |error| %>
+          <li><%= error.full_message %></li>
+        <% end %>
+      </ul>
+    </div>
+  <% end %>
+*/
